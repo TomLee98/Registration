25463c2ed13148d47f16fc5bf19b7e07f4ed00a7
TomLee98 <liweihanqq@163.com>
2024-02-06 22:57:34 +0800

    update

--- RegisterController/TaskControl/TaskParser/parse_oc_tc.m
+++ RegisterController/TaskControl/TaskParser/parse_oc_tc.m
@@ -1,5 +1,25 @@
 function tasks_ = parse_oc_tc(movtmpl_, regfrs_, volopt_, regopt_, batchsz_)
-%PARSE_TC_OC
+%PARSE_OC_TC This function parse registration algorithm and options to task
+%queue, where task can be passed to RegisterWorker object
+% Input:
+%   - movtmpl_:
+%   - regfrs_:
+%   - volopt_:
+%   - regopt_:
+%   - batchsz_:
+% Output:
+%   - tasks_:
+%
+% see also task, regtmpl, regopt
+
+arguments
+    movtmpl_    (1,1)   regtmpl
+    regfrs_     (1,:)   double {mustBePositive, mustBeInteger}
+    volopt_     (1,1)   struct
+    regopt_     (1,1)   regopt
+    batchsz_    (1,1)   double {mustBePositive, mustBeInteger}
+end
+
 
 
 
