277573511bcb897d452a9f0c4d4c482132c18b29
TomLee98 <liweihanqq@163.com>
2024-03-11 23:08:46 +0800

    .

--- Estimator/Estimator.m
+++ Estimator/Estimator.m
@@ -2,10 +2,11 @@ classdef Estimator < handle
     %SIGNALEXTRACTOR This class define a signal extractor object
 
     properties(Constant, Hidden)
+
     end
 
     properties(GetAccess=public, Dependent)
-        Activities
+        Activities      % get,  variable
 
     end
     
@@ -13,7 +14,7 @@ classdef Estimator < handle
         options         % 1-by-1 sigopt object
         channel         % 1-by-1 string, "r", "g" or "b"
         components      % 1-by-n nonnegtive integer array, component indices
-        activities      
+        activities      % n-by-t double array, row for components, colume for time step
     end
 
     properties(SetAccess=immutable, Hidden)
@@ -44,7 +45,26 @@ classdef Estimator < handle
             this.channel = ch_;
             this.components = comps_;
 
-            % 
+            fn = this.image_src.MetaData.frames;
+            acts = nan(numel(comps_), fn);
+            src_img = this.image_src;
+
+            % use parallel for speed up
+            parpool("Threads");
+
+            parfor n = 1:numel(comps_)
+                comps_n = comps_(n);
+
+                for t = 1:fn
+                    tmpvol = src_img.Movie()
+                end
+
+            end
+
+        end
+
+        function r = get.Activities(this)
+            r = this.activities;
         end
 
     end
