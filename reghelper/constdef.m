193f8cbce5ab61d77626540c7de3b34946390bec
TomLee98 <liweihanqq@163.com>
2025-06-02 13:58:50 +0800

    .

--- reghelper/constdef.m
+++ reghelper/constdef.m
@@ -4,6 +4,9 @@ classdef constdef
     properties (Access = public, Constant)
         GRAYSCALE_LIGHT = [0.94, 0.94, 0.94]
         GRAYSCALE_DARK =  [0.50, 0.50, 0.50]
+
+        GRAYSCALE_AXES_LIGHT = [1.00, 1.00, 1.00]
+        GRAYSCALE_AXES_DARK = [0.80, 0.80, 0.80]
     end
     
 end
